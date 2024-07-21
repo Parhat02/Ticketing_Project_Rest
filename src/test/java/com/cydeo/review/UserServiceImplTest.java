562318fd719757ded3f8ca67169c5a44769203fd
Ferhat <parhat002@gmail.com>
2024-07-21 20:06:18 +0300

    Test-Review

---
+++ src/test/java/com/cydeo/review/UserServiceImplTest.java
@@ -0,0 +1,130 @@
+package com.cydeo.review;
+
+import com.cydeo.dto.RoleDTO;
+import com.cydeo.dto.UserDTO;
+import com.cydeo.entity.Role;
+import com.cydeo.entity.User;
+import com.cydeo.mapper.UserMapper;
+import com.cydeo.repository.UserRepository;
+import com.cydeo.service.KeycloakService;
+import com.cydeo.service.ProjectService;
+import com.cydeo.service.TaskService;
+import com.cydeo.service.UserService;
+import com.cydeo.service.impl.UserServiceImpl;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceImplTest {
+
+    @Mock
+    private ProjectService projectService;
+    @Mock
+    private KeycloakService keycloakService;
+    @Mock
+    private TaskService taskService;
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private UserMapper userMapper;
+
+    @InjectMocks
+    private UserServiceImpl userService;
+
+    User user;
+    UserDTO userDTO;
+
+//    @BeforeAll
+//    public static void SetUpBeforeClass() throws Exception{
+//
+//    }
+//    @AfterAll
+//    public static void tearDownAfterClass() throws Exception{
+//
+//    }
+    @BeforeEach
+    public void setUp(){
+        user = new User();
+        user.setId(1L);
+        user.setFirstName("John");
+        user.setLastName("Doe");
+        user.setUserName("user");
+        user.setPassWord("Abc1");
+        user.setEnabled(true);
+        user.setRole(new Role("Manager"));
+
+        userDTO = new UserDTO();
+        userDTO.setId(1L);
+        userDTO.setFirstName("John");
+        userDTO.setLastName("Doe");
+        userDTO.setUserName("user");
+        userDTO.setPassWord("Abc1");
+        userDTO.setEnabled(true);
+
+        RoleDTO roleDTO = new RoleDTO();
+        roleDTO.setDescription("Manager");
+
+        userDTO.setRole(roleDTO);
+    }
+
+    private List<User> getUsers(){
+
+        User user2 = new User();
+        user2.setId(2L);
+        user2.setFirstName("Emily");
+
+        return List.of(user, user2);
+    }
+    private List<UserDTO> getUserDTOs(){
+
+        UserDTO userDTO2 = new UserDTO();
+        userDTO2.setId(2L);
+        userDTO2.setFirstName("Emily");
+
+        return List.of(userDTO, userDTO2);
+    }
+    @AfterEach
+    public void tearDown(){
+
+    }
+
+    @Test
+    public void test(){
+
+    }
+
+    @Test
+    public void should_list_all_users(){
+        // Given - Preparation
+        when(userRepository.findAllByIsDeletedOrderByFirstNameDesc(false)).thenReturn(getUsers());
+
+        when(userMapper.convertToDto(user)).thenReturn(userDTO);
+        when(userMapper.convertToDto(getUsers().get(1))).thenReturn(getUserDTOs().get(1));
+        //when(userMapper.convertToDto(any(User.class))).thenReturn(userDto, getUserDTOs().get(1)); // this line can replace the two lines above
+
+        List<UserDTO> expectedList = getUserDTOs();
+
+        // When - Action
+        List<UserDTO> actualList = userService.listAllUsers();
+
+        // Then - Assertion/Verification
+        //assertEquals(expectedList, actualList);
+
+        // AssertJ
+        assertThat(actualList).usingRecursiveComparison().isEqualTo(expectedList);
+
+        verify(userRepository, times(1)).findAllByIsDeletedOrderByFirstNameDesc(false);
+        verify(userRepository, never()).findAllByIsDeletedOrderByFirstNameDesc(true);
+    }
+
+}
